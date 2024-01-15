d500595b3f6f2af10e96fb134391fe8c0315de2a
juunaska <118882741+juunaska@users.noreply.github.com>
2024-01-15 15:23:12 +0200

    Add files via upload

---
+++ .config/fish/config.fish
@@ -0,0 +1,31 @@
+set fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+ export VISUAL=vim;
+ export EDITOR=vim;
+
+alias q="clear && neofetch"
+alias dd="clear"
+alias qqq="exit"
+alias sn="shutdown now"
+alias ll="ls -al"
+
+alias py="pyradio -t white_on_black"
+alias pipes="pipes.sh"
+alias hc="vim ~/.config/hypr/hyprland.conf"
+alias bc="vim ~/.config/bspwm/bspwmrc"
+alias fc="vim ~/.config/fish/config.fish"
+alias sweethome3d="/home/test/SweetHome3D-7.1/./SweetHome3D"
+alias neofetch="neofetch --ascii ~/.config/neofetch/ascii1.txt"
+alias sw="swww kill && swww init &"
+
+
+
+
+#----------------------------------------
+#alias cat="bat"
+#alias ls="lsd"
+#session!
