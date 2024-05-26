ac116a1880cf6fb79d1a5a592b386f31a82f4ae6
juunaska <118882741+juunaska@users.noreply.github.com>
2024-05-26 15:59:07 +0000

    Add files via upload

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,31 +1,57 @@
+#_______________________________________________
+#     ________ ___  ________  ___  ___     
+#    |\  _____\\  \|\   ____\|\  \|\  \    
+#    \ \  \__/\ \  \ \  \___|\ \  \\\  \   
+#     \ \   __\\ \  \ \_____  \ \   __  \  
+#      \ \  \_| \ \  \|____|\  \ \  \ \  \ 
+#       \ \__\   \ \__\____\_\  \ \__\ \__\
+#        \|__|    \|__|\_________\|__|\|__|
+#                     \|_________|         
+#________________________________________________
+
+
 set fish_greeting
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
- export VISUAL=vim;
- export EDITOR=vim;
+ export VISUAL=nvim;
+ export EDITOR=nvim;
+
+
+alias vim="nvim"
+alias cat="bat"
+alias ls="lsd"
 
 alias q="clear && neofetch"
+alias ff="fastfetch"
 alias dd="clear"
+alias ddls="clear && ls"
 alias qqq="exit"
 alias sn="shutdown now"
 alias ll="ls -al"
+alias pacman="sudo pacman"
 
 alias py="pyradio -t white_on_black"
 alias pipes="pipes.sh"
+
 alias hc="vim ~/.config/hypr/hyprland.conf"
 alias bc="vim ~/.config/bspwm/bspwmrc"
 alias fc="vim ~/.config/fish/config.fish"
+alias kc="vim ~/.config/kitty/kitty.conf"
+
 alias sweethome3d="/home/test/SweetHome3D-7.1/./SweetHome3D"
 alias neofetch="neofetch --ascii ~/.config/neofetch/ascii1.txt"
-alias sw="swww kill && swww init &"
+
+alias volup="pactl set-sink-volume 0 +20%"
+alias voldown="pactl set-sink-volume 0 -20%"
 
 
+alias brave="com.brave.Browser"
 
+xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
 
-#----------------------------------------
-#alias cat="bat"
-#alias ls="lsd"
-#session!
+#echo ""
+#fastfetch -l ~/.config/neofetch/fastfetch.txt
+#neofetch
