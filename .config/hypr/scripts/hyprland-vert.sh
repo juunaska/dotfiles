de86f5eb905bb3b4fa9342598492d1530f787b1e
test <juunaska@gmail.com>
2024-06-30 14:02:59 +0300

    asdasd

--- .config/hypr/scripts/hyprland-vert.sh
+++ .config/hypr/scripts/hyprland-vert.sh
@@ -1,2 +1,3 @@
-hyprctl keyword monitor eDP-1,preferred,auto,1,transform,1 &
-hyprctl keyword input:touchdevice:transform 1 &
+#hyprctl keyword monitor eDP-1,preferred,auto,1,transform,1 &
+hyprctl keyword monitor DSI-1,preferred,auto,1,transform,0 &
+hyprctl keyword input:touchdevice:transform 0 &
