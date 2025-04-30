9fd02f19629f3345a038454f3bbd2898160a6c4c
juunaska <juunaska@gmail.com>
2025-04-30 15:41:34 +0300

    ohyeas

--- installscript.sh
+++ installscript.sh
@@ -1,10 +1,2 @@
-
-pacman -S nvim fish git thunar base-devel flatpak shortwave kitty ranger rofi wofi 
-
-pacman -S xorg xorg-xinit hyprland waybar waypaper openbox polybar nitrogen gdm
-
-git clone https://aur.archlinux.org/yay.git
-git clone https://github.com/juunaska/dotfiles
-
-yay -S zen-browser niri lsd waypaper
+pacman -S nvim fish git base-devel flatpak thunar tumbler shortwave rnote loupe evince gparted kitty ranger rofi wofi xorg xorg-xinit hyprland waybar waypaper openbox polybar nitrogen gdm && git clone https://aur.archlinux.org/yay.git && git clone https://github.com/juunaska/dotfiles && cd yay && makepkg -si && yay -S niri lsd waypaper 
 
