59428760e0c8a77d22c31ecb244e64ba2dbdc6a0
juunaska <juunaska@gmail.com>
2025-04-30 11:18:39 +0300

    scriptt

---
+++ installscript.sh
@@ -0,0 +1,12 @@
+pacman -S nvim fish git thunar flatpak base-devel shortwave kitty xorg xorg-xinit gdm hyprland 
+
+
+pacman -S nvim fish git thunar base-devel flatpak shortwave kitty ranger 
+
+pacman -S xorg xorg-xinit hyprland gdm
+
+git clone https://aur.archlinux.org/yay.git
+git clone https://github.com/juunaska/dotfiles
+
+yay -S zen-browser niri
+
