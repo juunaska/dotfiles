a321001e0767e6ef20be1856d942799be6f91497
juunaska <juunaska@gmail.com>
2025-04-30 13:03:07 +0300

    aaa

--- installscript.sh
+++ installscript.sh
@@ -1,10 +1,10 @@
 
 pacman -S nvim fish git thunar base-devel flatpak shortwave kitty ranger rofi wofi 
 
-pacman -S xorg xorg-xinit hyprland gdm
+pacman -S xorg xorg-xinit hyprland waybar waypaper openbox polybar nitrogen gdm
 
 git clone https://aur.archlinux.org/yay.git
 git clone https://github.com/juunaska/dotfiles
 
-yay -S zen-browser niri
+yay -S zen-browser niri lsd waypaper
 
