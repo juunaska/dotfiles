628a4f605040439757a4dc6ee064e5bc91e81ced
juunaska <juunaska@gmail.com>
2025-05-03 19:25:07 +0300

    gagaga

--- .config/scripts/waybar/waybar-hide-niri.sh
+++ .config/scripts/waybar/waybar-hide-niri.sh
@@ -1,1 +1,1 @@
-pgrep waybar && killall waybar || waybar -c ~/.config/waybar/themes/colors/config -s ~/.config/waybar/themes/colors/lila.css 
+pgrep waybar && killall waybar || waybar -c ~/.config/waybar/themes/colors/config-niri -s ~/.config/waybar/themes/colors/niri.css
